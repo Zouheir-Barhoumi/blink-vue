e0fcafaa612b43d173deadb40b5fc5f8d2c4cd61
zouheir-barhoumi <zouheir.barhoumi@gmail.com>
2024-06-25 20:31:58 +0100

    completed the addContact logic

--- backend/controllers/usersController.mjs
+++ backend/controllers/usersController.mjs
@@ -34,22 +34,34 @@ const getUserContacts = async (req, res) => {
 };
 
 const addContact = async (req, res) => {
-  const userId = req.params.id;
-  const contactId = req.body.contactId;
+  try {
+    const userId = req.params.id;
+    const contactId = req.body.contactId;
 
-  const contact = await User.findById(contactId);
-  if (!contact) return res.status(404).json({ error: "User not found" });
+    const contact = await User.findById(contactId);
+    if (!contact) return res.status(404).json({ error: "User not found" });
 
-  // Check if the user is trying to add themselves
-  if (user.id === contact.id)
-    return res.status(400).json({ error: "Invalid request" });
+    // Check if the user is trying to add themselves
+    if (user.id === contact.id)
+      return res.status(400).json({ error: "Invalid request" });
 
-  const user = await User.findById(userId);
-  if (!user) return res.status(404).json({ error: "User not found" });
+    const user = await User.findById(userId);
+    if (!user) return res.status(404).json({ error: "User not found" });
 
-  // Check if the contact is already in the user's contacts
-  if (user.contacts.includes(contact.id)) {
-    return res.status(400).json({ error: "Contact already in contacts" });
+    // Check if the contact is already in the user's contacts
+    if (user.contacts.includes(contact.id)) {
+      return res.status(400).json({ error: "Contact already in contacts" });
+    }
+
+    user.contacts.push(contactId);
+    await user.save();
+
+    res
+      .status(201)
+      .json({ message: "Contact added successfully", contacts: user.contacts });
+  } catch (error) {
+    console.log(`Error adding contact: ${error}`);
+    return res.status(500).json({ error: error.message });
   }
 };
 
