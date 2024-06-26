ca787b0728f1b790b75fa1fb631b8a2211408117
zouheir-barhoumi <zouheir.barhoumi@gmail.com>
2024-06-26 16:01:57 +0100

    refactored contact format and addContact returns the same response as getUserContacts upon successful creation

--- backend/controllers/usersController.mjs
+++ backend/controllers/usersController.mjs
@@ -1,5 +1,14 @@
 import User from "../models/users.mjs";
 
+const formatContacts = (user) => {
+  return user.contacts.map((contact) => ({
+    _id: String(contact._id),
+    username: contact.username,
+    status: contact.status,
+    profilePicture: contact.profilePicture,
+  }));
+};
+
 const getUserDetails = async (req, res) => {
   try {
     const user = await user.findById(req.params.id).select("-password");
@@ -26,12 +35,7 @@ const getUserContacts = async (req, res) => {
     );
     if (!user) return res.status(404).json({ error: "User not found" });
 
-    const contacts = user.contacts.map((contact) => ({
-      _id: String(contact._id),
-      username: contact.username,
-      status: contact.status,
-      profilePicture: contact.profilePicture,
-    }));
+    const contacts = formatContacts(user);
 
     res.json(contacts);
   } catch (error) {
@@ -63,9 +67,18 @@ const addContact = async (req, res) => {
     user.contacts.push(contactId);
     await user.save();
 
+    const populatedUser = await User.findById(req.params.id).populate(
+      "contacts",
+      "-password",
+    );
+    if (!populatedUser)
+      return res.status(404).json({ error: "User not found" });
+
+    const contacts = formatContacts(populatedUser);
+
     res
       .status(201)
-      .json({ message: "Contact added successfully", contacts: user.contacts });
+      .json({ message: "Contact added successfully", contacts: contacts });
   } catch (error) {
     console.log(`Error adding contact: ${error}`);
     return res.status(500).json({ error: error.message });
