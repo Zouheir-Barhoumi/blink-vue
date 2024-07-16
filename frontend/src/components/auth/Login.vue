79e43241f3a4aa5e3de7b7db105b886f185d458f
zouheir-barhoumi <zouheir.barhoumi@gmail.com>
2024-07-16 20:57:23 +0100

    initial login form and installed vue-router

--- frontend/src/components/auth/Login.vue
+++ frontend/src/components/auth/Login.vue
@@ -1,15 +1,47 @@
+<template>
+  <div class="formWrapper">
+    <div class="form">
+      <h1 class="formTitle">Login</h1>
+      <form @submit.prevent="" method="post">
+        <div class="form__field">
+          <label for="email">Email</label>
+          <input type="email" id="email" v-model="form.email" />
+        </div>
+        <div class="form__field">
+          <label for="password">Password</label>
+          <input type="password" id="password" v-model="form.password" />
+        </div>
+        <div class="form__footer">
+          <div class="form__alt">
+            <small>You don't have an account?</small>
+            <router-link to="register">Signup</router-link>
+          </div>
+          <button type="submit">Login</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, reactive } from "vue";
 
 export default defineComponent({
+  methods: {
+    handleSubmit() {
+      alert("Form SUBMITTED!");
+    },
+  },
   setup() {
-    return {};
+    const form = reactive({
+      email: "",
+      password: "",
+    });
+    return {
+      form,
+    };
   },
 });
 </script>
-
-<template>
-  <div></div>
-</template>
 
 <style scoped></style>
