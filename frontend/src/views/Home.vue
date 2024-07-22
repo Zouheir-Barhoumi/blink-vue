d76ac505296e670b11ff939b73a89398158d063f
zouheir-barhoumi <zouheir.barhoumi@gmail.com>
2024-07-22 13:14:26 +0100

    header for homepage

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,5 +1,14 @@
 <template>
-  <p>Hello from home</p>
+  <div>
+    <div class="header">
+      <div class="user">
+        <div class="user__name">John Doe</div>
+        <div class="user__pfp">
+          <img src="https://placehold.co/50" alt="profile picture" />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -14,4 +23,48 @@ export default defineComponent({
 });
 </script>
 
-<style></style>
+<style scoped>
+.header {
+  /* padding: 0.5rem; */
+  border-radius: 0 0 1rem 1rem;
+  transform: translateY(0);
+  /* width: 100%; */
+  background: rgba(0, 0, 0, 0.5);
+  border-radius: 0 1rem 1rem 1rem;
+  position: relative;
+}
+.header::before {
+  content: "";
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background: var(--color-neon-blue);
+  filter: blur(200px);
+  z-index: -1;
+}
+.user {
+  display: flex;
+  justify-content: end;
+  align-items: center;
+  border-top: 1px solid var(--color-neon-green);
+  border-bottom: 1px solid var(--color-neon-green);
+  border-radius: 0 1rem 1rem 1rem;
+  /* padding: 0.8rem; */
+  background: rgba(57, 255, 20, 0.05);
+  height: 50px;
+  gap: 1rem;
+}
+.user__name {
+  font-size: 1.4em;
+}
+.user__pfp {
+  height: 50px;
+}
+img {
+  padding: 0;
+  margin: 0;
+  border-radius: 1rem;
+}
+</style>
