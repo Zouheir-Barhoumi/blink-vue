8588cbecc4fabfecfb599af45c23491bb0b3e75f
zouheir-barhoumi <zouheir.barhoumi@gmail.com>
2024-07-23 21:47:59 +0100

    renamed home

--- frontend/src/routes/routes.ts
+++ frontend/src/routes/routes.ts
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 
-import Home from "../views/Home.vue";
+import Chat from "../views/Chat.vue";
 import Login from "../views/Login.vue";
 import Register from "../views/Register.vue";
 
 const routes: Array<RouteRecordRaw> = [
-  { path: "/", component: Home },
+  { path: "/", component: Chat },
   { path: "/login", component: Login },
   { path: "/register", component: Register },
 ];
